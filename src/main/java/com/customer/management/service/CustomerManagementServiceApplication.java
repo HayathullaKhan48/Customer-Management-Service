eeb0f6c229729df4614eec77705898ba24e07ab9
HayathullaKhan48 <nayazihayathulla48@gmail.com>
2025-08-30 11:55:18 +0530

    first commit

---
+++ src/main/java/com/customer/management/service/CustomerManagementServiceApplication.java
@@ -0,0 +1,14 @@
+package com.customer.management.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerManagementServiceApplication.class, args);
+		System.out.println("Customer Management Service Application is Successfully executing...");
+	}
+
+}
